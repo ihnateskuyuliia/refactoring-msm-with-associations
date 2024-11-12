82105480f124439ad9af2a3dc4ce7225cce6c258
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-11-12 23:59:18 +0000

    g

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,7 +12,11 @@
 #  updated_at  :datetime         not null
 #  director_id :integer
 #
+
 class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
+
+  belongs_to(:director, class_name: "Director", foreign_key: "director_id")
+  has_many(:characters, class_name: "Character", foreign_key: "movie_id")
 end
